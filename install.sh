fc0b9f8512a0727e606dbfa5da165022f4cb5c37
gstux <marcogas.2724@gmail.com>
2018-01-25 18:37:27 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
 cd ~
 git clone https://github.com/gstux/aurpkg
 cd aurpkg
-mv ~/aurpkg/aurpkg /bin
-mv ~/aurpkg/aurpkg-install /bin
-mv ~/aurpkg/aurpkg-config /bin
-chmod +x /bin/aurpkg
-chmod +x /bin/aurpkg-install
-chmod +x /bin/aurpkg-config
+sudo mv ~/aurpkg/aurpkg /bin
+sudo mv ~/aurpkg/aurpkg-install /bin
+sudo mv ~/aurpkg/aurpkg-config /bin
+sudo chmod +x /bin/aurpkg
+sudo chmod +x /bin/aurpkg-install
+sudo chmod +x /bin/aurpkg-config
 cd ~
 rm -rf ~/aurpkg
 aurpkg-config
