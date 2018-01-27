92697a0124d53fb92b26ae4c562b2e994488a48b
Davide Garberi <dade.garberi@gmail.com>
2018-01-27 22:07:22 +0100

    install: Use cp instead of mv
    Signed-off-by: Davide Garberi <dade.garberi@gmail.com>

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 cd ~
 git clone https://github.com/gstux/aurpkg
 cd aurpkg
-sudo mv ~/aurpkg/aurpkg /bin
-sudo mv ~/aurpkg/aurpkg-install /bin
-sudo mv ~/aurpkg/aurpkg-config /bin
+sudo cp ~/aurpkg/aurpkg /bin
+sudo cp ~/aurpkg/aurpkg-install /bin
+sudo cp ~/aurpkg/aurpkg-config /bin
 sudo chmod +x /bin/aurpkg
 sudo chmod +x /bin/aurpkg-install
 sudo chmod +x /bin/aurpkg-config
