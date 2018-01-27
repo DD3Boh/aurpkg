769af8ab7dde5f526c7178d0e8d0870184eb54c4
Davide Garberi <dade.garberi@gmail.com>
2018-01-27 22:07:52 +0100

    Use aurpkg* instead of calling files individually
    Signed-off-by: Davide Garberi <dade.garberi@gmail.com>

--- install.sh
+++ install.sh
@@ -1,12 +1,8 @@
 cd ~
 git clone https://github.com/gstux/aurpkg
 cd aurpkg
-sudo cp ~/aurpkg/aurpkg /bin
-sudo cp ~/aurpkg/aurpkg-install /bin
-sudo cp ~/aurpkg/aurpkg-config /bin
-sudo chmod +x /bin/aurpkg
-sudo chmod +x /bin/aurpkg-install
-sudo chmod +x /bin/aurpkg-config
+sudo cp ~/aurpkg/aurpkg* /bin
+sudo chmod +x /bin/aurpkg*
 cd ~
 rm -rf ~/aurpkg
 echo "done"
